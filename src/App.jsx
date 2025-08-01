b2951b35871d1c408fe7825fec4a4727fb2fcdb6
hasin.pramodya <pramodyahasin@gmail.com>
2025-08-01 19:35:29 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
