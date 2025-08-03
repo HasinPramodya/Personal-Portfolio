167f604075867fd879e2a3a612b8b47b057dbb12
hasin.pramodya <pramodyahasin@gmail.com>
2025-08-03 23:52:27 +0530

    feature : add the star background componentfor the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import ThemeToggle from "../components/ThemeToggle.jsx";
+import StarBackground from "@/components/StarBackground.jsx";
 
 export default function Home() {
     return(
         <>
             <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-
+                {/*ThemeToggle*/}
             <ThemeToggle/>
 
             {/*Background Effects*/}
 
+                <StarBackground/>
 
             {/*Navabar*/}
 
