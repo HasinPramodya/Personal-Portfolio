ad151bae3e59f94efcb567ac7664fdc2f0f71c61
hasin.pramodya <pramodyahasin@gmail.com>
2025-08-06 19:43:30 +0530

    fix : update NavBar component for proper navigation ali9gnment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const navItem = [
     return(
         <>
          <nav className={cn("fixed w-full z-40 transition-all duration-300",isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5")}>
-             <div className="container flex items-center justify-between">
+             <div className="container  flex items-center justify-evenly">
                  <a className="text-xl font-bold text-primary flex items-center" href="#hero">
                      <span className={"relative z-10"}>
                          <span className={"text-glow text-foreground"}>Hasin Pramodya</span>{" "}Portfolio
