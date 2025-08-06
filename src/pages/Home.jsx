28ef36ab8726cd18b1c456511607b543b5b13113
hasin.pramodya <pramodyahasin@gmail.com>
2025-08-06 17:16:06 +0530

    feature : add all new component to Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,11 @@ import StarBackground from "@/components/StarBackground.jsx";
 import Navbar from "@/components/Navbar.jsx";
 import HeroSection from "@/components/HeroSection.jsx";
 import AboutMeSection from "@/components/AboutMeSection.jsx";
+import SkillsSection from "@/components/SkillsSection.jsx";
+import ProjectSection from "@/components/ProjectSection.jsx";
+import ContactSection from "@/components/ContactSection.jsx";
+import Footer from "@/components/Footer.jsx";
+
 
 export default function Home() {
     return(
@@ -23,10 +28,15 @@ export default function Home() {
                 <main>
                     <HeroSection/>
                     <AboutMeSection/>
+                    <SkillsSection/>
+                    <ProjectSection/>
+                    <ContactSection/>
+
                 </main>
 
 
             {/*Footer*/}
+                <Footer/>
             </div>
         </>
     )
